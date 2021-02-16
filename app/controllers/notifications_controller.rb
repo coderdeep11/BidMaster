543ed63dc71f0d67fe6936617bed57e8e47068cf
coderdeep11 <deepakds7699@gamil.com>
2021-02-16 18:59:22 +0530

    now users can send attachments to each other

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,5 +3,6 @@ class NotificationsController < ApplicationController
 
   def index
     @notifications = Notification.where(user_id: current_user.id)
+    @conversation = Conversation.where('sender_id =? OR recipient_id =?', current_user.id, current_user.id).first
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
   validates_presence_of :content
+  has_many_attached :documents
 end
