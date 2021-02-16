a2ed289e4729d1f7d05e7c293bae73b1913c41a6
coderdeep11 <deepakds7699@gamil.com>
2021-02-16 23:28:01 +0530

    added previews to images and pdfs

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
-  validates_presence_of :content
+  validates :content, presence: true, if: :documents?
   has_many_attached :documents
+
+  def documents?
+    documents.empty?
+  end
 end
